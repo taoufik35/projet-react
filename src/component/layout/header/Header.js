55188e70a9c49498c3d3d5ef21fe92baf0ca2f04
taoufik35 <79227708+taoufik35@users.noreply.github.com>
2021-04-29 23:53:27 +0200

    add header

---
+++ src/component/layout/header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import header from './header.png'; 
+
+
+function Header() {
+  // Import result is the URL of your image
+  return <img src={header} alt="header" />;
+}
+
+export default Header;
