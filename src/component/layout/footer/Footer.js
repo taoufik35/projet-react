e75a140bd58577f06b9e08e1d5cee90b432c14b6
taoufik35 <79227708+taoufik35@users.noreply.github.com>
2021-04-30 00:20:59 +0200

    add footer

---
+++ src/component/layout/footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import footer from './footer.png'; 
+
+
+function Footer() {
+
+  // Import result is the URL of your image
+  return (
+        <div className= "image">
+        <img  src={footer} alt="footer" />
+        </div>
+  )
+       
+  
+  
+}
+
+export default Footer;
