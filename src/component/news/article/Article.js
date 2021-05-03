ad14186db23f521a15831676e433a4bbf0884f05
unknown <aitdra.taoufik@gmail.com>
2021-05-03 12:10:24 +0200

    final

--- src/component/news/article/Article.js
+++ src/component/news/article/Article.js
@@ -1,6 +1,6 @@
 function Article (props) {
     return(
-      <div className="col-6 col-sm-4 col-lg-3" key={props.index}>
+      <div className="col-6 col-sm-4 col-lg-3" >
         <article className= "bg-dark text-center text-danger py-5 my-2 ">
             {props.index}
         </article>
